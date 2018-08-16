dd920df0bd2e943b4cc9274ac34b4e44c983c888
Jeury Mejia <jeury.mejia@gmail.com>
2018-08-15 23:56:52 -0400

    Create shell script to compile, link and run java programs

---
+++ comp-build-run.sh
@@ -0,0 +1,19 @@
+if [[ $# != 1 ]]; then
+  echo "Usage: ./comp-build-run.sh <week-#>"
+  exit
+fi
+
+# entering the folder passed
+cd /programming-exercises/$1
+echo "Currently in..."
+pwd
+
+# getting java files
+java_files=`ls | grep ".java"`
+
+echo "Compiling..."
+# compiling every file
+for file in $java_files; do
+  javac -cp "../../algs4.jar" $file
+  echo $file "compiled!"
+done
