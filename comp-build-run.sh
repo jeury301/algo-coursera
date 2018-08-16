7e08d9fd08bc28587affc34d02302d73344b9921
Jeury Mejia <jeurymejia@nypl.org>
2018-08-16 09:14:39 -0400

    Complete comp-build-run to run java programs

--- comp-build-run.sh
+++ comp-build-run.sh
@@ -1,4 +1,4 @@
-if [[ $# != 1 ]]; then
+if [[ $# < 1 ]]; then
   echo "Usage: ./comp-build-run.sh <week-#>"
   exit
 fi
@@ -17,3 +17,16 @@ for file in $java_files; do
   javac -cp "../../algs4.jar" $file
   echo $file "compiled!"
 done
+
+echo "Running..."
+# runnnig selected file or every file
+if [[ $# == 2 ]]; then
+  echo "Java file:" $2
+  java $2
+else
+  for file in $java_files; do
+    file_name=`echo $file | cut -d'.' -f1`
+    echo "Java file:" $file_name
+    java $file_name
+  done
+fi
